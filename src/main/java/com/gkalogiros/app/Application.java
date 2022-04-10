bd00a8f12f12065e1133ef2a9274502ac8a7476c
Georgios Kalogiros <kalogiros.2011@my.bristol.ac.uk>
2022-04-10 19:26:24 +0100

    Second Commit

--- src/main/java/com/gkalogiros/app/Application.java
+++ src/main/java/com/gkalogiros/app/Application.java
@@ -1,4 +1,4 @@
-package com.gkalogiros.feedserver;
+package com.gkalogiros.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
